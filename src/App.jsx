10cfe49624df4e3bd063231ab9d50f9c3c09fafa
alejandro-programador2 <programador2@booksandbooksdigital.com.co>
2022-08-30 08:44:28 -0500

    first commit í

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "components/buttons/Button";
+
+export const App = () => {
+   return (
+      <>
+         <Button label="#43302b" primary />
+
+         <p className="u-text-primary-900">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis perferendis, cumque sint ea, sed fugit voluptate explicabo ipsum quibusdam
+            quia dolorum voluptates deleniti quaerat aspernatur? Optio saepe fugit
+         </p>
+      </>
+   );
+};
